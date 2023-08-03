818dd6ae2bae3c682354d73f6f27e129f818f7e4
shubh <shubhvjain@gmail.com>
2023-08-03 14:39:45 +0200

    entsoe apis for forecast total and wind solar working

--- entsoeAPI.py
+++ entsoeAPI.py
@@ -48,12 +48,72 @@ def entsoe_getActualGenerationDataPerProductionType(options={"country": "", "sta
     return {"data":data1,"duration":durationMin}
 
 
+
+def entsoe_getDayAheadAggregatedGeneration(options={"country": "", "start": "", "end": ""}):
+    client = entsoePandas(api_key=getAPIToken())
+    data = client.query_generation_forecast(options["country"], start=pd.Timestamp(options["start"], tz='UTC'), end=pd.Timestamp(options["end"], tz='UTC'))
+    if isinstance(data,pd.Series):
+        data = data.to_frame(name="Actual Aggregated")
+    
+    data['startTimeIndex'] = data.index.strftime('%Y%m%d%H%M')
+    data['startTimeIndex'] = pd.to_datetime(data['startTimeIndex'])
+
+    duration = data['startTimeIndex'].iloc[1] - data['startTimeIndex'].iloc[0]
+    durationMin = duration.seconds // 60
+    timeStampsUTC = util.countIntervals(options["start"],options["end"],durationMin)    
+    data = data.head(timeStampsUTC["count"])
+    data["startTime"] = timeStampsUTC["startBin"]
+    data["endTime"] = timeStampsUTC["endBin"]
+    
+    newCol = {'Actual Aggregated': 'total'}
+    data.rename(columns=newCol, inplace=True)
+
+    if DEBUG:   
+      fileName= options["country"]+"-"+options["start"]+"-"+options["end"]+"-"+str(durationMin)+'-forecast-total-raw'
+      data.to_csv("./test/"+fileName+".csv")
+    
+    data = data.reset_index(drop=True)
+
+    return {"data":data,"duration":durationMin}
+
+
+def entsoe_getDayAheadGenerationForecastsWindSolar(options={"country": "", "start": "", "end": ""}):
+    client = entsoePandas(api_key=getAPIToken())
+    data = client.query_wind_and_solar_forecast(options["country"],  start=pd.Timestamp(options["start"], tz='UTC'), end=pd.Timestamp(options["end"], tz='UTC'))
+
+    data['startTimeIndex'] = data.index.strftime('%Y%m%d%H%M')
+    data['startTimeIndex'] = pd.to_datetime(data['startTimeIndex'])    
+
+    duration = data['startTimeIndex'].iloc[1] - data['startTimeIndex'].iloc[0]
+    durationMin = duration.seconds // 60
+
+    timeStampsUTC = util.countIntervals(options["start"],options["end"],durationMin)
+    data = data.head(timeStampsUTC["count"])
+    data["startTime"] = timeStampsUTC["startBin"]
+    data["endTime"] = timeStampsUTC["endBin"]
+
+    validCols = ["Solar","Wind Offshore","Wind Onshore"]
+    existingCol = []
+    for col in validCols:
+        if col in data.columns:
+            existingCol.append(col)
+    data["totalRenewable"] = data[existingCol].sum(axis=1)
+    
+    data = data.reset_index(drop=True)
+
+    if DEBUG:   
+      fileName= options["country"]+"-"+options["start"]+"-"+options["end"]+"-"+str(durationMin)+'-forecast-wind-solar-raw'
+      data.to_csv("./test/"+fileName+".csv")
+
+    return {"data":data,"duration":durationMin}
+
+
 def getActualRenewableValues(options={"country":"","start":"","end":"", "interval60":True}):
     totalRaw = entsoe_getActualGenerationDataPerProductionType(options)
     total = totalRaw["data"]
     duration = totalRaw["duration"]
     if options["interval60"] == True & totalRaw["duration"] != 60 :
-      print("need to be converted")
+      print("Data will to be converted to 60 min interval")
       table = util.convertTo60MinInterval(totalRaw,options["start"],options["end"])
       duration = 60
     else: 
@@ -76,3 +136,24 @@ def getActualRenewableValues(options={"country":"","start":"","end":"", "interva
     table['percentRenewableWS'].fillna(0, inplace=True)
     table["percentRenewableWS"] = table["percentRenewableWS"].round().astype(int)
     return {"data":table,"duration":duration}
+
+
+def getRenewableForecast(options={"country": "", "start": "", "end": "" }):
+    # print(options)
+    totalRaw = entsoe_getDayAheadAggregatedGeneration(options)
+    if totalRaw["duration"] != 60 :
+        total = util.convertTo60MinInterval(totalRaw,options["start"],options["end"])
+    else :
+        total = totalRaw["data"]
+    
+    windsolarRaw = entsoe_getDayAheadGenerationForecastsWindSolar(options)
+    if  windsolarRaw["duration"] != 60 :
+        windsolar = util.convertTo60MinInterval(windsolarRaw,options["start"],options["end"])
+    else :
+        windsolar = windsolarRaw["data"]   
+  
+    windsolar["total"] = total["total"]
+    windsolar["percentRenewable"] = (windsolar['totalRenewable'] / windsolar['total']) * 100
+    windsolar['percentRenewable'].fillna(0, inplace=True)
+    windsolar["percentRenewable"] = windsolar["percentRenewable"].round().astype(int)
+    return {"data":windsolar,"duration":60}
--- getData.py
+++ getData.py
@@ -14,16 +14,34 @@ def test():
     option6 = {"start":"202301010000","end":"202301050000","country":"DE","interval60":False}
     option7 = {"start":"202301010000","end":"202301050000","country":"ES","interval60":True}
     option8 = {"start":"202301010000","end":"202301050000","country":"ES","interval60":False}
-    test1 = e.getActualRenewableValues(option7)
-    test1["data"].to_csv("./test/"+optionToFile(option7)+"-"+str(test1["duration"])+".csv")
+    
+    # test1 = e.getActualRenewableValues(option7)
+    # test1["data"].to_csv("./test/"+optionToFile(option7)+"-"+str(test1["duration"])+".csv")
 
-    test2 = e.getActualRenewableValues(option8)
-    test2["data"].to_csv("./test/"+optionToFile(option8)+"-"+str(test2["duration"])+".csv")
+    # test2 = e.getActualRenewableValues(option8)
+    # test2["data"].to_csv("./test/"+optionToFile(option8)+"-"+str(test2["duration"])+".csv")
     
     # a = e.getActualRenewableValues(option1)
     # a.to_csv("./test/"+optionToFile(option1)+".csv")
     # b = e.getActualRenewableValues(option2)
     # b.to_csv("./test/"+optionToFile(option2)+".csv")
+
+    # t1 = e.entsoe_getDayAheadAggregatedGeneration(option1)
+    # t1 = e.entsoe_getDayAheadAggregatedGeneration(option1)
+    # t1 = e.entsoe_getDayAheadAggregatedGeneration(option2)
+
+    #t2 = e.entsoe_getDayAheadGenerationForecastsWindSolar(option2)
+    #t3 = e.entsoe_getDayAheadGenerationForecastsWindSolar(option3)
+
+    # ct = getCountryList()
+    # for c in ct :
+    #     print()        
+
+    # test3 = e.getRenewableForecast(option5)
+    # test3["data"].to_csv("./test/"+optionToFile(option5)+"-forecast-"+str(test3["duration"])+".csv")
+
+    # test4 = e.getRenewableForecast(option6)
+    # test4["data"].to_csv("./test/"+optionToFile(option6)+"-forecast-"+str(test4["duration"])+".csv")
 
 
 test()
