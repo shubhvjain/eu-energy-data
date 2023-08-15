55ce2352d5ab1a81da4f89089fde1c23e0de7600
shubh <shubhvjain@gmail.com>
2023-08-15 03:52:47 +0200

    test code

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import entsoeAPI as e
 import pandas as pd
+from entsoe import EntsoePandasClient as entsoePandas
+from entsoe import EntsoeRawClient as eRaw
+import os
 
 def generateIntialFileName(options,type):
     f = options["country"]+"-"+options["start"]+"-"+options["end"]+"-"+type
@@ -24,18 +27,6 @@ def saveHistoricalActualData(options):
 # saveHistoricalActualData({"start":"202204010000","end":"202204200000","country":"FR","interval60":True})
 # saveHistoricalActualData({"start":"202201010000","end":"202301010000","country":"FR","interval60":True})
 
-
-
-
-# To verfiy if the data download is correct 
-"""
-Country 1 : France. Data for 2020 downloaded manually from the ENTSOE portal
-
-"""
-from entsoe import EntsoePandasClient as entsoePandas
-from entsoe import EntsoeRawClient as eRaw
-import os
-
 def getAPIToken():
   variable_name = "ENTSOE_TOKEN"
   value = os.environ.get(variable_name)
@@ -45,9 +36,6 @@ def getAPIToken():
 
 
 def checkActualFranc():
-    #print(options)
-    #print(pd.Timestamp(options["start"], tz='UTC'))
-    #print(pd.Timestamp(options["end"], tz='UTC'))
     oneDay = pd.Timedelta(days=1)
     #endDayPlus1 = pd.Timestamp(options["end"], tz='UTC') + oneDay
     client1 = entsoePandas(api_key=getAPIToken())
@@ -81,5 +69,59 @@ def checkValueFrance2022():
 # data = e.entsoe_getDayAheadAggregatedGeneration({"start":"202305010000","end":"202305150000","country":"FR","interval60":False})
 # print(data)
 
-# data1 = e.entsoe_getDayAheadGenerationForecastsWindSolar({"start":"202305010000","end":"202305150000","country":"FR","interval60":False})
+# data1 = e.entsoe_getDayAheadGenerationForecastsWindSolar({"start":"202007010000","end":"202007250000","country":"FR","interval60":False})
+# print(data1["data"])
+# data1["data"].to_csv("./test/sample.csv")
+# data1 = e.entsoe_getDayAheadGenerationForecastsWindSolar({"start":"202004040000","end":"202004060000","country":"LV","interval60":True})
 # print(data1)
+
+# data1 = e.getRenewableForecast({"start":"202305100000","end":"202305200000","country":"GR","interval60":True})
+# print(data1)
+
+
+
+# def refineData(options,data1):
+#   durationMin = (data1.index[1] - data1.index[0]).total_seconds() / 60
+#   timeStampsUTC = util_countIntervals(options["start"],options["end"],durationMin)
+
+#   logging.info("  Row count : Fetched =  "+str(len(data1))+" , Required = "+str(timeStampsUTC["count"]))
+#   logging.info("  Duration : "+str(durationMin))
+#   totalAverageValue = data1.mean().fillna(0).round().astype(int)
+
+#   data1['startTimeIndex'] = data1.index.strftime('%Y%m%d%H%M')
+#   data1['startTimeIndex'] = pd.to_datetime(data1['startTimeIndex'])
+#   data1.to_csv("./test/"+"testrefine1"+".csv")
+#   start_time = data1.index.min()
+#   end_time = data1.index.max()
+#   expected_timestamps = pd.date_range(start=start_time, end=end_time, freq=f"{durationMin}T")
+#   expected_df = pd.DataFrame(index=expected_timestamps)
+#   missing_indices = expected_df.index.difference(data1.index)
+#   logging.info("  Missing values ("+str(len(missing_indices))+") :"+str(missing_indices))
+#   for index in missing_indices:
+#     logging.info("    Missing value: "+str(index))
+#     rows_same_day = data1[ data1.index.date == index.date()]    
+#     if len(rows_same_day)>0 :
+#       avg_val = rows_same_day.mean().fillna(0).round().astype(int)
+#       avg_type = "average day value "+ str(rows_same_day.index[0].date())+" "
+#     else:
+#       avg_val = totalAverageValue
+#       avg_type = "whole data average "
+#     logging.info("      replaced with "+avg_type+" : "+' '.join(avg_val.astype(str)))
+#     new_row = pd.DataFrame([avg_val], columns=data1.columns, index=[index])
+#     data1 = pd.concat([data1, new_row])            
+#     # prev_index = index - dur
+#     # next_index = index + dur
+#     # avg_val = (data1.loc[prev_index]+data1.loc[next_index])/2
+#     # logging.info("      previous value: " + ' '.join(data1.loc[prev_index].astype(str)))
+#     # logging.info("      previous value: " + ' '.join(data1.loc[next_index].astype(str)))
+#     # logging.info("      average value: " + ' '.join(avg_val.astype(str)))
+#     # new_row = pd.DataFrame([avg_val], columns=data1.columns, index=[index])
+#     # data1 = pd.concat([data1, new_row])
+#   data1.sort_index(inplace=True)
+#   data1.to_csv("./test/"+"testrefine2"+".csv")
+#   print(data1)
+#   d = pd.DataFrame({"start": timeStampsUTC["startBin"] ,"end": timeStampsUTC["endBin"]})
+#   d.to_csv("./test/datetest.csv")
+#   data1["startTime"] = timeStampsUTC["startBin"]
+#   data1["endTime"] = timeStampsUTC["endBin"]
+#   return data1
