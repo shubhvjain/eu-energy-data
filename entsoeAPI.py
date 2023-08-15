ebf541f2892488cba4177f8598063974a34f9d23
shubh <shubhvjain@gmail.com>
2023-08-15 03:44:39 +0200

    hotfix in refining data

--- entsoeAPI.py
+++ entsoeAPI.py
@@ -5,7 +5,7 @@ from entsoe import EntsoePandasClient as entsoePandas
 import os
 
 import logging
-logging.basicConfig(filename='entsoe3.log', format='%(asctime)s - %(levelname)s - %(message)s',level="INFO")
+logging.basicConfig(filename='entsoe.log', format='%(asctime)s - %(levelname)s - %(message)s',level="INFO")
 
 DEBUG=True
 
@@ -39,13 +39,17 @@ def util_convertTo60MinInterval(rawData, start, end):
         # determing how many rows need to be combined to get data in 60 min format. The rows are com
         groupingFactor = int(60/duration)
         oldData = rawData["data"]
-        dataColToRemove = ['startTime', 'endTime']
+        oldData["startTime"] = pd.to_datetime(oldData['startTime'])
+        start_time = oldData["startTime"] .min()
+        end_time = oldData["startTime"] .max()        
+        durationMin=60
+        expected_timestamps = pd.date_range(start=start_time, end=end_time, freq=f"{durationMin}T",tz='UTC')
+        expected_timestamps = expected_timestamps.strftime('%Y%m%d%H%M')
+        dataColToRemove = ['startTime']
         oldData = oldData.drop(dataColToRemove, axis=1)
         oldData['group_id'] = oldData.index // groupingFactor
         newGroupedData = oldData.groupby('group_id').mean()
-        timeUTCInterval = util_countIntervals(start, end, 60)
-        newGroupedData["startTime"] = timeUTCInterval["startBin"]
-        newGroupedData["endTime"] = timeUTCInterval["endBin"]
+        newGroupedData["startTime"] = expected_timestamps
         return newGroupedData
 
 
@@ -58,9 +62,7 @@ def getAPIToken():
 
 def refineData(options,data1):
   durationMin = (data1.index[1] - data1.index[0]).total_seconds() / 60
-  timeStampsUTC = util_countIntervals(options["start"],options["end"],durationMin)
-
-  logging.info("  Row count : Fetched =  "+str(len(data1))+" , Required = "+str(timeStampsUTC["count"]))
+  logging.info("  Row count : Fetched =  "+str(len(data1)))
   logging.info("  Duration : "+str(durationMin))
 
   start_time = data1.index.min()
@@ -69,13 +71,20 @@ def refineData(options,data1):
   expected_df = pd.DataFrame(index=expected_timestamps)
   missing_indices = expected_df.index.difference(data1.index)
   logging.info("  Missing values ("+str(len(missing_indices))+"):"+str(missing_indices))
+  totalAverageValue = data1.mean().fillna(0).round().astype(int)
   for index in missing_indices:
     logging.info("    Missing value: "+str(index))
-    rows_same_day = data1[ data1.index.date == index.date()]
-    avg_val = rows_same_day.mean().fillna(0).round().astype(int)
-    logging.info("      replaced with day average value of "+str(rows_same_day.index[0].date())+" : "+' '.join(avg_val.astype(str)))
+    rows_same_day = data1[ data1.index.date == index.date()]    
+    if len(rows_same_day)>0 :
+      avg_val = rows_same_day.mean().fillna(0).round().astype(int)
+      avg_type = "average day value "+ str(rows_same_day.index[0].date())+" "
+    else:
+      avg_val = totalAverageValue
+      avg_type = "whole data average "
+    logging.info("      replaced with "+avg_type+" : "+' '.join(avg_val.astype(str)))
     new_row = pd.DataFrame([avg_val], columns=data1.columns, index=[index])
-    data1 = pd.concat([data1, new_row])            
+    data1 = pd.concat([data1, new_row]) 
+
     # prev_index = index - dur
     # next_index = index + dur
     # avg_val = (data1.loc[prev_index]+data1.loc[next_index])/2
@@ -84,10 +93,10 @@ def refineData(options,data1):
     # logging.info("      average value: " + ' '.join(avg_val.astype(str)))
     # new_row = pd.DataFrame([avg_val], columns=data1.columns, index=[index])
     # data1 = pd.concat([data1, new_row])
+  data1['startTime'] = (data1.index.tz_convert('UTC')).strftime('%Y%m%d%H%M')
   data1.sort_index(inplace=True)
   
-  data1["startTime"] = timeStampsUTC["startBin"]
-  data1["endTime"] = timeStampsUTC["endBin"]
+
   return data1
 
 def entsoe_getActualGenerationDataPerProductionType(options={"country": "", "start": "", "end": ""}):
@@ -114,14 +123,13 @@ def entsoe_getActualGenerationDataPerProductionType(options={"country": "", "sta
 
 
 def entsoe_getDayAheadAggregatedGeneration(options={"country": "", "start": "", "end": ""}):
+    logging.info("DayAheadForecastsTotal")
     logging.info(options)
     client = entsoePandas(api_key=getAPIToken())
     data = client.query_generation_forecast(options["country"], start=pd.Timestamp(options["start"], tz='UTC'), end=pd.Timestamp(options["end"], tz='UTC'))
     if isinstance(data,pd.Series):
         data = data.to_frame(name="Actual Aggregated")
     durationMin = (data.index[1] - data.index[0]).total_seconds() / 60
-    timeStampsUTC = util_countIntervals(options["start"],options["end"],durationMin)    
-    data = data.head(timeStampsUTC["count"])
     refinedData = refineData(options,data)
     newCol = {'Actual Aggregated': 'total'}
     refinedData.rename(columns=newCol, inplace=True)
@@ -135,28 +143,25 @@ def entsoe_getDayAheadAggregatedGeneration(options={"country": "", "start": "", 
 
 
 def entsoe_getDayAheadGenerationForecastsWindSolar(options={"country": "", "start": "", "end": ""}):
+    logging.info("DayAheadForecastsWindSolar")
     logging.info(options)
     client = entsoePandas(api_key=getAPIToken())
     data = client.query_wind_and_solar_forecast(options["country"],  start=pd.Timestamp(options["start"], tz='UTC'), end=pd.Timestamp(options["end"], tz='UTC'))
-
     durationMin = (data.index[1] - data.index[0]).total_seconds() / 60
-    timeStampsUTC = util_countIntervals(options["start"],options["end"],durationMin)
-    data = data.head(timeStampsUTC["count"])
-    refinedData = refineData(options,data) 
+    refinedData1 = refineData(options,data) 
     validCols = ["Solar","Wind Offshore","Wind Onshore"]
     existingCol = []
     for col in validCols:
-        if col in refinedData.columns:
+        if col in refinedData1.columns:
             existingCol.append(col)
-    refinedData["totalRenewable"] = refinedData[existingCol].sum(axis=1)
-    
-    refinedData = data.reset_index(drop=True)
+    refinedData1["totalRenewable"] = refinedData1[existingCol].sum(axis=1)
+    refinedData1 = refinedData1.reset_index(drop=True)
 
     if DEBUG:   
       fileName= options["country"]+"-"+options["start"]+"-"+options["end"]+"-"+str(durationMin)+'-forecast-wind-solar-raw'
-      refinedData.to_csv("./test/"+fileName+".csv")
+      refinedData1.to_csv("./test/"+fileName+".csv")
 
-    return {"data":refinedData,"duration":durationMin}
+    return {"data":refinedData1,"duration":durationMin}
 
 
 def getActualRenewableValues(options={"country":"","start":"","end":"", "interval60":True}):
